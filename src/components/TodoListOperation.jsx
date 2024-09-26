3411d11123f15db32123120ec66cf8f024240f56
MdMostakimBillah <mostakimbillahn609@gmail.com>
2024-09-26 20:37:13 +0600

    store data founded need to fix towmorrow Installah

--- src/components/TodoListOperation.jsx
+++ src/components/TodoListOperation.jsx
@@ -2,22 +2,20 @@ import { useEffect, useState } from "react";
 import classes from "../styles/TodoListOperation.module.css";
 import SingleTask from "./SingleTask";
 const TodoListOperation = ({ workingSector, sectoreIndex, listData }) => {
-  const [list, setList] = useState([]);
-  // the props have an object
+  const [list, setList] = useState(() => {
+    const savedList = localStorage.getItem("db");
+    return savedList ? JSON.parse(savedList) : listData;
+  });
   const [sector, setSector] = useState(() => {
     const sotoredData = localStorage.getItem("seleteSector");
     return sotoredData ? JSON.parse(sotoredData) : [];
   });
   const [sectorIndex, setSectorIndex] = useState();
   const [newInput, setNewInput] = useState("");
-  const [allTask, setAllTask] = useState([]);
+
   //checking the clicked value is available in whole array or note
   const clickedData = list.filter((title) => title.name === sector.name);
   const showingClickdata = clickedData.map((item) => item)[0];
-
-  useEffect(() => {
-    setAllTask(showingClickdata);
-  }, [showingClickdata]);
 
   //input hander
   const inputHanlder = (e) => {
@@ -27,19 +25,27 @@ const TodoListOperation = ({ workingSector, sectoreIndex, listData }) => {
   //submit handlr
   const handleSumit = (e) => {
     e.preventDefault();
-    setAllTask((prevValue) => ({
-      ...prevValue,
-      allChildTasks: [...prevValue.allChildTasks, newInput], //store new task
-    }));
+    const updated = list.map((item) =>
+      item.name === sector.name
+        ? { ...item, allChildTasks: [...item.allChildTasks, newInput] }
+        : item
+    );
+
+    setList(updated); //here adding and updating my data and add some inpur in array but how to store it local storage when i refresh the page the data will remove
     setNewInput("");
   };
 
-  //store in local state
+  //update list
   useEffect(() => {
-    localStorage.setItem("task", JSON.stringify(allTask));
-  }, [allTask]);
+    localStorage.setItem("db", JSON.stringify(list));
+  }, [list]);
 
-  // console.log(allTask);
+  // Set list data in state on initial mount
+  // useEffect(() => {
+  //   if (listData && list.length === 0) {
+  //     setList(listData);
+  //   }
+  // }, [listData]);
 
   // set list data in a state
   useEffect(() => {
@@ -72,9 +78,16 @@ const TodoListOperation = ({ workingSector, sectoreIndex, listData }) => {
             <h3>Task List</h3>
           </div>
           <div className={classes.taskesWraper}>
-            <SingleTask />
-            <SingleTask />
-            <SingleTask />
+            {list.map((item, index) =>
+              item.name === sector.name
+                ? item.allChildTasks.map((task, i) => (
+                    <SingleTask task={task} key={i} />
+                  ))
+                : ""
+            )}
+
+            {/* <SingleTask />
+            <SingleTask /> */}
           </div>
         </div>
         <div className={classes.ProcessingList}>
@@ -84,9 +97,7 @@ const TodoListOperation = ({ workingSector, sectoreIndex, listData }) => {
             </span>
             <h3>Processing</h3>
           </div>
-          <div className={classes.taskesWraper}>
-            <SingleTask />
-          </div>
+          <div className={classes.taskesWraper}>{/* <SingleTask /> */}</div>
         </div>
         <div className={classes.CompleteList}>
           <div className={classes.topTitle}>
@@ -94,8 +105,8 @@ const TodoListOperation = ({ workingSector, sectoreIndex, listData }) => {
             <h3>Complete</h3>
           </div>
           <div className={classes.taskesWraper}>
-            <SingleTask />
-            <SingleTask />
+            {/* <SingleTask />
+            <SingleTask /> */}
           </div>
         </div>
         {/* <div className={classes.Delete}>
