2bff970b94386a2675adc81ac80be34322075676
MdMostakimBillah <mostakimbillahn609@gmail.com>
2024-09-26 14:34:37 +0600

    add input task functionality and neet to store all task data in local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import classes from "../styles/Home.module.css";
 import Navigation from "./Navigation";
 import TodoListOperation from "./TodoListOperation";
 const Home = () => {
+  //get selected sector in this state variable
+  const [sectore, setSector] = useState([]);
+  const [sectoreIndex, setSectorIndex] = useState();
+
+  const [list, setList] = useState([]);
+  //selected sectior store in state variable function
+  const selectedSector = (item, index) => {
+    setSector(item);
+    setSectorIndex(index);
+  };
+
+  const listStore = (item) => {
+    setList(item);
+  };
+
   return (
     <div className={classes.container}>
-      <Navigation />
-      <TodoListOperation />
+      <Navigation SectorHanlder={selectedSector} listStore={listStore} />
+      <TodoListOperation
+        workingSector={sectore}
+        sectoreIndex={sectoreIndex}
+        listData={list}
+      />
     </div>
   );
 };
--- src/components/TodoListOperation.jsx
+++ src/components/TodoListOperation.jsx
@@ -1,6 +1,68 @@
+import { useEffect, useState } from "react";
 import classes from "../styles/TodoListOperation.module.css";
 import SingleTask from "./SingleTask";
-const TodoListOperation = () => {
+const TodoListOperation = ({ workingSector, sectoreIndex, listData }) => {
+  const [list, setList] = useState([]);
+  // the props have an object
+  const [sector, setSector] = useState(() => {
+    const sotoredData = localStorage.getItem("seleteSector");
+    return sotoredData ? JSON.parse(sotoredData) : [];
+  });
+  const [sectorIndex, setSectorIndex] = useState();
+  const [newInput, setNewInput] = useState("");
+  const [allTask, setAllTask] = useState([]);
+  //checking the clicked value is available in whole array or note
+  const clickedData = list.filter((title) => title.name === sector.name);
+  const showingClickdata = clickedData.map((item) => item)[0];
+
+  useEffect(() => {
+    setAllTask(showingClickdata);
+  }, [showingClickdata]);
+
+  //input hander
+  const inputHanlder = (e) => {
+    setNewInput(e.target.value);
+  };
+
+  //submit handlr
+  const handleSumit = (e) => {
+    e.preventDefault();
+    setAllTask((prevValue) => ({
+      ...prevValue,
+      allChildTasks: [...prevValue.allChildTasks, newInput], //store new task
+    }));
+    setNewInput("");
+  };
+
+  //store in local state
+  useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(allTask));
+  }, [allTask]);
+
+  // console.log(allTask);
+
+  // set list data in a state
+  useEffect(() => {
+    setList(listData);
+  }, [listData]);
+
+  //index selected sector
+  useEffect(() => {
+    setSectorIndex(sectoreIndex);
+  }, [sectoreIndex]);
+
+  //data store this variable state
+  useEffect(() => {
+    if (workingSector && Object.keys(workingSector).length > 0) {
+      setSector(workingSector);
+    }
+  }, [workingSector]);
+
+  //store data in local storage
+  useEffect(() => {
+    localStorage.setItem("seleteSector", JSON.stringify(sector));
+  }, [sector]);
+
   return (
     <>
       <div className={classes.WraperListOperation}>
@@ -10,8 +72,6 @@ const TodoListOperation = () => {
             <h3>Task List</h3>
           </div>
           <div className={classes.taskesWraper}>
-            <SingleTask />
-            <SingleTask />
             <SingleTask />
             <SingleTask />
             <SingleTask />
@@ -26,7 +86,6 @@ const TodoListOperation = () => {
           </div>
           <div className={classes.taskesWraper}>
             <SingleTask />
-            <SingleTask />
           </div>
         </div>
         <div className={classes.CompleteList}>
@@ -37,9 +96,6 @@ const TodoListOperation = () => {
           <div className={classes.taskesWraper}>
             <SingleTask />
             <SingleTask />
-            <SingleTask />
-
-            <SingleTask />
           </div>
         </div>
         {/* <div className={classes.Delete}>
@@ -48,30 +104,42 @@ const TodoListOperation = () => {
           <h3>Delete</h3>
         </div>
       </div> */}
-      </div>
-      {/*adding data input*/}
-      <div className={classes.taskAddInput}>
-        <form action="">
-          <div className={classes.wraperAddTask}>
-            <div className={classes.inputField}>
-              <input type="text" placeholder="Add Task" />
-            </div>
-            <div className={classes.actionButton}>
-              <button type="submit" className={classes.addTaskBtn}>
-                <span className="material-symbols-outlined">add</span>
-              </button>
-              <div className={classes.selectedSection}>
-                <p value="No Section Selected">No Section Selected</p>
+
+        {/*adding data input*/}
+        <div className={classes.taskAddInput}>
+          <form action="" onSubmit={handleSumit}>
+            <div className={classes.wraperAddTask}>
+              <div className={classes.inputField}>
+                <input
+                  type="text"
+                  placeholder="Add Task"
+                  value={newInput}
+                  onChange={inputHanlder}
+                  disabled={showingClickdata && sector.name ? false : true}
+                  required
+                />
+              </div>
+              <div className={classes.actionButton}>
+                <button type="submit" className={classes.addTaskBtn}>
+                  <span className="material-symbols-outlined">send</span>
+                </button>
+                <div className={classes.selectedSection}>
+                  <p>
+                    {showingClickdata && sector.name
+                      ? sector.name
+                      : "No Sector Selected"}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
 
       {/*delete box */}
-      <div className={classes.deleteBox}>
+      {/* <div className={classes.deleteBox}>
         <span className="material-symbols-outlined">delete</span>
-      </div>
+      </div> */}
     </>
   );
 };
