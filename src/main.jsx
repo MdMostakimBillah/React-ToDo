f82e2995223b1687597dc0ef99300f6ca4c25cc5
MdMostakimBillah <mostakimbillahn609@gmail.com>
2024-11-27 20:50:38 +0600

    PWA done

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/App.css";
+//pwa
+import { registerSW } from "virtual:pwa-register";
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content is available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,54 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: [
+    "favicon.ico",
+    "apple-touch-icon.png",
+    "android-chrome-512x512.png",
+  ],
+  manifest: {
+    name: "Note Book",
+    short_name: "Note",
+    description: "An app that can add your all note.",
+    icons: [
+      {
+        src: "./android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "./android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#272d39",
+    background_color: "#272d39",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), VitePWA(manifestForPlugin)],
+//   base: "/React-ToDo/",
+// });
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      ...manifestForPlugin,
+      devOptions: {
+        enabled: true, // Enables PWA in development for debugging
+      },
+    }),
+  ],
   base: "/React-ToDo/",
 });
