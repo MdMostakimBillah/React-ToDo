bd1b349189ba27b0e40fc1a4d5d96ac7f921f1c4
MdMostakimBillah <mostakimbillahn609@gmail.com>
2024-11-29 17:11:01 +0600

    font related change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5i6muugaa"
+    "revision": "0.e2040afodkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
